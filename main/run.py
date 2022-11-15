6c13a7283815c2cf7ecc9a58e62a5c14078c30ec
Hakameikito <thomaxus@gmail.com>
2022-11-15 18:34:11 +0100

    Almost done with fetching the final image

--- main/run.py
+++ main/run.py
@@ -7,39 +7,50 @@ from PIL import Image, ImageTk
 from io import BytesIO
 
 
-def get_soup_from_link(page: int = 0, res_x: int = 1920, res_y: int = 1080):
-    base = f"https://anime-pictures.net/pictures/view_posts/{page}?res_x={res_x}&res_y={res_y}&lang=en"
-    content = requests.get(base).content
-    soup = BeautifulSoup(content, "lxml")  # # get base link and transform it to soup
-    return soup
-
-
 # get total amount of available pages
 # go to page chosen from total scope, i.e. 0 - max
 # look for all the posts, choose one at random and return final link to the img post
 def get_rndm_img_page(res_x: int = 1920, res_y: int = 1080):
+    def get_soup_from_link(page: int = 0, res_x: int = 1920, res_y: int = 1080):
+        base = f"https://anime-pictures.net/pictures/view_posts/{page}?res_x={res_x}&res_y={res_y}&lang=en"
+        content = requests.get(base).content
+        soup = BeautifulSoup(content, "lxml")  # # get base link and transform it to soup
+        return soup
+
     soup = get_soup_from_link(res_x=res_x, res_y=res_y)
-    pages = [page.string for page in soup.body.p.contents if page.string.isdigit()]  # all pages
+    pages = [page.string for page in soup.body.p.contents if page.string.isdigit()]       # all pages
 
     rndm_page = get_soup_from_link(randint(0, int(pages[-1])), res_x=res_x, res_y=res_y)  # roll a page
     img_links = []
     for img_link in rndm_page.find_all('a'):
-        i = img_link.get("href")  # get all links
-        if "view_post" in i and i[21] != "0" and "view_posts" not in i:  # skip falsy links
-            index = i[21:i.find("?")]  # get ID of the image
-            if (index, i) not in img_links:  # skip duplicates
+        i = img_link.get("href")                                             # get all links
+        if "view_post" in i and i[21] != "0" and "view_posts" not in i:      # skip falsy links
+            index = i[21:i.find("?")]                                        # get ID of the image
+            if (index, i) not in img_links:                                  # skip duplicates
                 img_links.append((index, i))
 
-    random_link_appendix = img_links[randint(0, len(img_links))]  # roll 1 random image
+    random_link_appendix = img_links[randint(0, len(img_links))]          # roll 1 random image
     random_link = "https://anime-pictures.net" + random_link_appendix[1]  # make final link
     return random_link
 
 
 def get_image(link: str):
-    soup = get_soup_from_link(link)  # TODO: implement image return
-    pass
+    content = requests.get(link).content
+    soup = BeautifulSoup(content, "lxml")
+    pictures = soup.find_all("picture")
+    pic_link = pictures[-1].img.get("src")                   # //cdn.anime-pictures.net/previews/1c7/1c758f126d84b8b7991eb3d62db8ea8f_bp.jpg
+    print(pic_link) # TODO: fix the bug with wrong extension when more than 1 img on the page
+    pic_link = pic_link[pic_link.find("previews/") + 8::]   # /1c7/1c758f126d84b8b7991eb3d62db8ea8f_bp.jpg
+    extension = pic_link[pic_link.find(".")::]              # get file extension, i.e. jpg/png
+    pic_link = pic_link[:pic_link.find("_")]                # /1c7/1c758f126d84b8b7991eb3d62db8ea8f
+    final_link = f"https://images.anime-pictures.net{pic_link}{extension}"
+    print(final_link)
+    print(extension)
 
-get_rndm_img_page(3200, 5000)
+
+link = get_rndm_img_page(1920, 1080)
+print(link)
+get_image(link)
 
 
 # test_img = "https://images.anime-pictures.net/686/6860ac3e69d773d950679c3e473575d2.png?if=ANIME-PICTURES.NET_-_777379-1883x4500-re%3Azero+kara+hajimeru+isekai+seikatsu-white+fox-echidna+%28re%3Azero%29-jun+%28aousa0328%29-single-long+hair.png"
